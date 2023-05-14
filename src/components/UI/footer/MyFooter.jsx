28c9141abd1e54f689910742a8c573f1fa74047b
frexxx-7 <borzenkokiril9@gmail.com>
2023-05-14 17:11:40 +0300

    Noted add

--- src/components/UI/footer/MyFooter.jsx
+++ src/components/UI/footer/MyFooter.jsx
@@ -8,11 +8,6 @@ export default function MyFooter() {
 
   return (
     <footer className={classes.footer}>
-      <div className={classes.footer_social_networks}>
-        <p className={classes.footer_social_networks_p}>Social networks:</p>
-        <a href="https://instagram.com/frex.x.x7?igshid=YzdkMWQ2MWU="><img src={`./instagram-${theme}.png`} alt="Instagram" className={classes.footer_social_networks_img_instagram} /></a>
-        <a href="https://vk.com/frexxx_7"><img src={`./vk-${theme}.png`} alt="VKontakte" className={classes.footer_social_networks_img_vk} /></a>
-      </div>
 
       <div className={classes.footer_name}>
         <p className={classes.footer_name_p}>©Progrim 2023</p>
